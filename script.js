8565b932421d0dda5fd20b62e73ee50c956f8e80
Madan patakota <pmohanreddy@firstam.com>
2022-06-24 18:36:58 +0530

    Basics

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+console.log("madan");
+console.log("mohan");
+
+
+console.log(document.getElementById("paragraph"));
+console.log(document.getElementsByClassName("paragraph1"));
+
+
+document.getElementsByClassName("paragraph1")[0].innerHTML = "Madan";
+document.getElementsByClassName("paragraph1")[1].innerHTML = "Mohan";
+document.getElementsByClassName("paragraph1")[2].innerHTML = "Reddy";
+
+document.getElementById("container").innerText = "Hi how are you";
+document.getElementById("container").innerText = '<label style="background-color:red">Printed</label>';
+// document.getElementById("container").innerHTML = '<label style="background-color:red">Printed</label>';
+
+document.getElementsByClassName("paragraph1")[0].innerText = "Hi How are you";
+document.getElementsByClassName("paragraph1")[0].innerHTML = "Hi How are you";
+
+//document.
+
+
+// document.append("<p>Paragraph</p>")
+
+
+//document.getElementById("madan").is
+
+
